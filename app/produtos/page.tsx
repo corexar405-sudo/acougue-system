312f44dc83d09d0b94a28e0184dfa3faca212a84
Lucas <Corexar405@gmail.com>
2026-05-19 15:41:32 -0300

    layout admin corrigido

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -179,10 +179,10 @@ Obrigado pela preferencia!
 
       <header className="bg-zinc-950 border-b border-zinc-800 sticky top-0 z-50">
 
-        <div className="flex items-center gap-4">
+      <div className="flex items-center gap-4">
 
   <a
-    href="/admin"
+    href="/login"
     className="
       bg-zinc-800
       px-4
