f4c1dbdcce4ec5585bdf9d5ae395ead02398859a
Lucas <Corexar405@gmail.com>
2026-05-19 15:39:48 -0300

    login admin

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,19 +5,21 @@ import { supabase } from "../../lib/supabase"
 import { useRouter } from "next/navigation"
 
 export default function Login() {
-  const [email, setEmail] = useState("")
-  const [senha, setSenha] = useState("")
 
   const router = useRouter()
 
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+
   async function fazerLogin() {
+
     const { error } = await supabase.auth.signInWithPassword({
       email,
       password: senha,
     })
 
     if (error) {
-      alert("Email ou senha inválidos")
+      alert("Login inválido")
       return
     }
 
@@ -25,37 +27,55 @@ export default function Login() {
   }
 
   return (
-    <main className="min-h-screen bg-black flex items-center justify-center">
-      <div className="bg-zinc-900 p-10 rounded-2xl w-[400px]">
-        <h1 className="text-4xl font-bold text-red-600 mb-8 text-center">
-          Login Admin
+    <main className="min-h-screen bg-black flex items-center justify-center p-6">
+
+      <div className="bg-zinc-900 p-10 rounded-3xl w-full max-w-md">
+
+        <h1 className="text-5xl font-black text-red-600 mb-8 text-center">
+          Admin
         </h1>
 
-        <div className="space-y-4">
+        <div className="space-y-5">
+
           <input
             type="email"
             placeholder="Email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="bg-zinc-800 p-4 rounded-xl w-full text-white"
+            onChange={(e) =>
+              setEmail(e.target.value)
+            }
+            className="w-full bg-zinc-800 p-4 rounded-2xl text-white"
           />
 
           <input
             type="password"
             placeholder="Senha"
             value={senha}
-            onChange={(e) => setSenha(e.target.value)}
-            className="bg-zinc-800 p-4 rounded-xl w-full text-white"
+            onChange={(e) =>
+              setSenha(e.target.value)
+            }
+            className="w-full bg-zinc-800 p-4 rounded-2xl text-white"
           />
 
           <button
             onClick={fazerLogin}
-            className="bg-red-600 w-full py-4 rounded-xl font-bold text-white"
+            className="
+              bg-red-600
+              hover:bg-red-700
+              w-full
+              py-4
+              rounded-2xl
+              font-black
+              text-xl
+            "
           >
             Entrar
           </button>
+
         </div>
+
       </div>
+
     </main>
   )
 }
